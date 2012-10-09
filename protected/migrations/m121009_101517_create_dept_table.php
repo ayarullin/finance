80ec753bcb4d023ce7dfd27c840fdcc34dd32c69
Azat Yarullin <azat.yarullin@gmail.com>
2012-10-09 16:04:59 -0400

    debt migration

---
+++ protected/migrations/m121009_101517_create_dept_table.php
@@ -0,0 +1,19 @@
+<?php
+
+class m121009_101517_create_dept_table extends CDbMigration
+{
+	public function up()
+	{
+		$this->createTable('debt', array(
+			'id' => 'pk',
+			'sum' => 'decimal(10,2)',
+			'date_time' => 'datetime',
+			'description' => 'string'
+		));
+	}
+
+	public function down()
+	{
+		$this->dropTable('debt');
+	}
+}
