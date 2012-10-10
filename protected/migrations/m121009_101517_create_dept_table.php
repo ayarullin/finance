492b13be231e661b481e3553075f08886d1a15e6
Azat Yarullin <azat.yarullin@gmail.com>
2012-10-10 03:52:44 -0400

    model generated

--- protected/migrations/m121009_101517_create_dept_table.php
+++ protected/migrations/m121009_101517_create_dept_table.php
@@ -8,7 +8,8 @@ class m121009_101517_create_dept_table extends CDbMigration
 			'id' => 'pk',
 			'sum' => 'decimal(10,2)',
 			'date_time' => 'datetime',
-			'description' => 'string'
+			'description' => 'string',
+			'done' => 'boolean'
 		));
 	}
 
---
+++ protected/models/Debt.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * This is the model class for table "debt".
+ *
+ * The followings are the available columns in table 'debt':
+ * @property integer $id
+ * @property string $sum
+ * @property string $date_time
+ * @property string $description
+ * @property boolean $done
+ */
+class Debt extends CActiveRecord
+{
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return Debt the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'debt';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('sum', 'length', 'max'=>10),
+            array('description', 'length', 'max'=>255),
+            array('date_time, done', 'safe'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, sum, date_time, description, done', 'safe', 'on'=>'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+        );
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'sum' => 'Sum',
+            'date_time' => 'Date Time',
+            'description' => 'Description',
+            'done' => 'Done',
+        );
+    }
+
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
+
+        $criteria=new CDbCriteria;
+
+        $criteria->compare('id',$this->id);
+        $criteria->compare('sum',$this->sum,true);
+        $criteria->compare('date_time',$this->date_time,true);
+        $criteria->compare('description',$this->description,true);
+        $criteria->compare('done',$this->done);
+
+        return new CActiveDataProvider($this, array(
+            'criteria'=>$criteria,
+        ));
+    }
+}
